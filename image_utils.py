20b9f33ef3ffd99448977a13d70c7ccd5977aeb5
319129714 <roishvimer@gmail.com>
2026-01-07 12:52:18 +0200

    Update image_utils.py

--- image_utils.py
+++ image_utils.py
@@ -6,4 +6,30 @@ def load_image(path):
     pass # Replace the `pass` with your code
 
 def edge_detection(image):
-    pass # Replace the `pass` with your code
+roi = plt.imread('roi.jpeg')
+plt.imshow(roi)
+plt.axis('off')
+plt.show()
+print(roi.shape)
+print(roi.dtype)
+gray_roi = np.mean(roi, axis=2)
+
+plt.imshow(gray_roi, cmap='gray')
+plt.axis('off')
+plt.show()
+
+print("Grayscale image shape:", gray_roi.shape)
+print("Grayscale image dtype:", gray_roi.dtype)
+
+from scipy.signal import convolve2d
+filter = np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]])
+filter = filter / np.sum(filter)
+filtered_image = convolve2d(gray_roi, filter, mode='same')
+fig, axs = plt.subplots(1,2)
+axs[0].imshow(gray_roi, cmap='gray')
+axs[0].axis('off')
+axs[0].set_title('Original Image')
+axs[1].imshow(filtered_image, cmap='gray')
+axs[1].axis('off')
+axs[1].set_title('Filtered Image')
+plt.show()
